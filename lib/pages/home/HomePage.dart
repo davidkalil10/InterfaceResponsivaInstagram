df426277909e6fde968303069b2e54f1132fc0da
David Kalil Braga <dmmd10@hotmail.com>
2021-03-10 02:15:13 -0300

    iniciando appbar responsiva

--- lib/pages/home/HomePage.dart
+++ lib/pages/home/HomePage.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_instagram/pages/home/widgets/ResponsiveAppBar.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Teste"),
-      ),
+      appBar:PreferredSize(
+        preferredSize: Size(double.infinity,52),
+        child:  ResponsiveAppBar(),
+      )
     );
   }
 }
---
+++ lib/pages/home/widgets/ResponsiveAppBar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class ResponsiveAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.black,
+      elevation: 1,
+      title: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: 1000),
+        child: Row(
+          children: [
+            MouseRegion(
+              cursor: SystemMouseCursors.click,
+              child: Text(
+                "Flutter",
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                    fontFamily: "Billabong",
+                    fontWeight: FontWeight.w500),
+              ),
+            ),
+            Container(
+              width: 200,
+              height: 30,
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.white)),
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(left: 4),
+              child: Row(
+                children: [
+                  Icon(Icons.search,color: Colors.white,size: 15,),
+                  SizedBox(width: 4,),
+                  Expanded(
+                    child: TextField(
+                      style: TextStyle(color: Colors.white, fontSize: 14),
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          isCollapsed: true,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
